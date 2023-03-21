1f1436a16a263e7d5b35773b903ab9dfa7518146
nagahole <61644420+nagahole@users.noreply.github.com>
2023-03-22 08:10:59 +1100

    Polynomial and maths wip

--- src/maths.c
+++ src/maths.c
@@ -4,14 +4,22 @@
 
 double flatland_sin(float x)
 {
-    // OPTIONAL: TODO
+    double res = 0;
+    double sign = 1;
+
+    for (int power = 1; power <= 20; power += 2, sign = -sign) 
+    {
+        res += sign * (flatland_pow(x, power) / flatland_factorial(power));
+    }
+
+    return res;
+
     return x;
 }
 
 double flatland_cos(float x)
 {
-    // OPTIONAL: TODO
-    return 1 - x;
+    return flatland_sin(x + PI / 2);
 }
 
 
@@ -19,4 +27,41 @@ double flatland_sqrt(float x)
 {
     // OPTIONAL: TODO
     return 1;
+}
+
+double flatland_pow(float base, int power) 
+{
+    if (power == 0)
+        return 1;
+
+    float absolute = 1;
+
+    for (int i = 0; i < (int) flatland_abs(power); i++) 
+    {
+        absolute *= base;
+    }
+
+    if (power < 0)
+        return 1.0 / absolute;
+    else
+        return absolute;
+}
+
+double flatland_abs(float n) 
+{
+    if (n > 0)
+        return n;
+    else;
+        return -n;
+}
+
+double flatland_factorial(int n) 
+{
+    double res = 1;
+
+    for (int i = n; i > 0; i--) {
+        res *= i;
+    }
+
+    return res;
 }
--- src/polynomial.c
+++ src/polynomial.c
@@ -7,16 +7,41 @@ void polynomial_create(
         int n_elements,
         float* elements)
 {
-    // TODO
+    /* Data is in the format of: 
+     * - First 4 bytes: int denoting number of coefficients
+     * - Bytes in consecutive chunks of 4: Coefficients of elements
+     */
+    ((int*) data)[0] = n_elements;
+
+    for (int i = 0; i < n_elements; i++) 
+    {
+        ((float*) data)[i + 1] = elements[i];
+    }
+
     return;
 }
 
 /*
  * Polynomial evaluate
- *
  */
 float polynomial_evaluate(void* polynomial, int t)
 {
-    // TODO
-    return 0;
+    int n_elements = ((int*) polynomial)[0];
+
+    float res = 0;
+
+    for (int i = 0; i < n_elements; i++) 
+    {
+        float coefficient = ((float*) polynomial)[i + 1];
+        float buf = coefficient;
+
+        //Basically pow(polynomial[i + 1], n_elements - 1)
+        for (int j = 0; j < n_elements - 2 - j; j++) {
+            buf *= coefficient;
+        }
+
+        res += buf;
+    }
+
+    return res;
 }
