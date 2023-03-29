37f145610a2033ba26c7d582cfb3ab469dd74f2b
nagahole <61644420+nagahole@users.noreply.github.com>
2023-03-29 17:17:54 +1100

    Pre Derivative Solution

---
+++ test/lib/test_high_order.h
@@ -0,0 +1,13 @@
+#ifndef TEST_HIGH_ORDER_H
+#define TEST_HIGH_ORDER_H 1
+
+#include "test.h"
+
+#include "asteroid.h" 
+#include "asteroid_cluster.h"
+#include "flatland.h"
+
+int test_high_order();
+double n_random();
+
+#endif
